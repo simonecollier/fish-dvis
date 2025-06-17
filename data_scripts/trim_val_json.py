f4f4197085c43e3899f90f64a00770a9d59a5cfc
simonecollier <simonekcollier@gmail.com>
2025-06-17 10:32:17 -0400

    change val trimming to output frames in middle of video

--- data_scripts/trim_val_json.py
+++ data_scripts/trim_val_json.py
@@ -1,35 +1,39 @@
 import json
 
-VAL_JSON_PATH = "/data/fishway_ytvis/val_long.json"  # Change this if your file is elsewhere
+VAL_JSON_PATH = "/data/fishway_ytvis/val_all.json"  # Change this if your file is elsewhere
 OUTPUT_JSON_PATH = "/data/fishway_ytvis/val.json"  # Output file
 
 MAX_FRAMES = 30
 
-def truncate_list(lst, n):
-    """Truncate a list to at most n elements."""
-    if isinstance(lst, list):
-        return lst[:n]
-    return lst
+def get_middle_indices(total, n):
+    """Get the start and end indices for the middle n elements of a list of length total."""
+    if total <= n:
+        return 0, total
+    start = (total - n) // 2
+    end = start + n
+    return start, end
 
 def main():
     with open(VAL_JSON_PATH, "r") as f:
         data = json.load(f)
 
-    # Truncate videos
+    # Map video_id to (start, end) indices for slicing
+    video_id_to_indices = {}
     for video in data["videos"]:
-        if len(video["file_names"]) > MAX_FRAMES:
-            video["file_names"] = video["file_names"][:MAX_FRAMES]
-            video["length"] = MAX_FRAMES
-        else:
-            video["length"] = len(video["file_names"])
+        total_frames = len(video["file_names"])
+        start, end = get_middle_indices(total_frames, MAX_FRAMES)
+        video["file_names"] = video["file_names"][start:end]
+        video["length"] = len(video["file_names"])
+        video_id_to_indices[video["id"]] = (start, end)
 
     # Truncate annotations
     for ann in data["annotations"]:
+        vid = ann["video_id"]
+        start, end = video_id_to_indices[vid]
         # Truncate segmentations, bboxes, areas if present and are lists
         for key in ["segmentations", "bboxes", "areas"]:
             if key in ann and isinstance(ann[key], list):
-                if len(ann[key]) > MAX_FRAMES:
-                    ann[key] = ann[key][:MAX_FRAMES]
+                ann[key] = ann[key][start:end]
         # Update length if present
         if "length" in ann:
             ann["length"] = min(ann.get("length", MAX_FRAMES), MAX_FRAMES)
