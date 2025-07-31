2e09cf0e72735764e51fa1ce0dfc8442646f7a51
simonecollier <simonekcollier@gmail.com>
2025-07-31 17:26:19 -0400

    script for creating masked data

---
+++ labeling_scripts/run_mask.sh
@@ -0,0 +1,5 @@
+python fish-dvis/labeling_scripts/create_mask_video.py \
+    --annotations_file shared-data/fishway_ytvis/val_trimmed.json \
+    --output_dir /data/fishway_ytvis/val_mask_imgs_trimmed \
+    --degradation_type blur \
+    --degradation_factor 0
