5b02d80a10c20499d1b5e26c97b7a883091357c8
simonecollier <simonekcollier@gmail.com>
2025-11-10 11:18:41 -0500

    update model being tested for attention extraction

--- attention_analysis/eval_run_attn.sh
+++ attention_analysis/eval_run_attn.sh
@@ -6,7 +6,7 @@ export PYTHONPATH=/home/simone/fish-dvis/DVIS_Plus/DVIS_DAQ
 # To enable debug prints, add --debug to the command line
 python /home/simone/fish-dvis/attention_analysis/train_net_video_eval_attn.py \
   --num-gpus 1 \
-  --config-file /home/simone/store/simone/dvis-model-outputs/trained_models/model_silhouette_lr5e-4_redo/config.yaml  \
+  --config-file /home/simone/store/simone/dvis-model-outputs/trained_models/model_camera_ft123/config.yaml  \
   --eval-only \
-  MODEL.WEIGHTS /home/simone/store/simone/dvis-model-outputs/trained_models/model_silhouette_lr5e-4_redo/model_0003635.pth \
-  OUTPUT_DIR /home/simone/store/simone/dvis-model-outputs/trained_models/model_silhouette_lr5e-4_redo/attn_extract_0003635_eval
+  MODEL.WEIGHTS /home/simone/store/simone/dvis-model-outputs/trained_models/model_camera_ft123/model_0003231.pth \
+  OUTPUT_DIR /home/simone/store/simone/dvis-model-outputs/trained_models/model_camera_ft123/attn_extract_3231
