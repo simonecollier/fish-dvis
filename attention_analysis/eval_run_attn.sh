d7da729a0c10a983da4dba11b7c01380f9e63015
simonecollier <simonekcollier@gmail.com>
2025-11-19 13:35:13 -0500

    Revert "script for performing evaluation on scrambled val sets"
    This reverts commit bccabd68ec39e162fa2a136bf980d0e4c8118b60.

--- attention_analysis/eval_run_attn.sh
+++ attention_analysis/eval_run_attn.sh
@@ -6,7 +6,7 @@ export PYTHONPATH=/home/simone/fish-dvis/DVIS_Plus/DVIS_DAQ
 # To enable debug prints, add --debug to the command line
 python /home/simone/fish-dvis/attention_analysis/train_net_video_eval_attn.py \
   --num-gpus 1 \
-  --config-file /home/simone/store/simone/dvis-model-outputs/trained_models/model_camera_fold4/config.yaml  \
+  --config-file /home/simone/store/simone/dvis-model-outputs/trained_models/model_camera_ft123/config.yaml  \
   --eval-only \
-  MODEL.WEIGHTS /home/simone/store/simone/dvis-model-outputs/trained_models/model_camera_fold4/model_0006059.pth \
-  OUTPUT_DIR /home/simone/store/simone/dvis-model-outputs/trained_models/model_camera_fold4/eval_attn_6059_split3
+  MODEL.WEIGHTS /home/simone/store/simone/dvis-model-outputs/trained_models/model_camera_ft123/model_0003231.pth \
+  OUTPUT_DIR /home/simone/store/simone/dvis-model-outputs/trained_models/model_camera_ft123/attn_extract_3231
